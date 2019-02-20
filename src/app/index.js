954b445475c17eade364397a9b76f8156123ee4e
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-02-20 21:19:35 +0200

    added main body

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <header className="Header">
         <h1>Vaiva Gr</h1>
         <hr />
@@ -35,7 +35,78 @@ function App() {
           src="https://static1.squarespace.com/static/5a26bc7451a584c4421507c7/t/5a28b3f8652deabe5e26f57c/1512780740388/1d83e0f607b7b1de607fff558a573b6c--cat-smile-smile-smile.jpg"
         />
       </header>
-    </div>
+      <main>
+        <section className="Section">
+          <div className="Section--left">
+            <h3>Education</h3>
+          </div>
+          <div className="Section--right">
+            <h3>CodeAcademy</h3>
+            <p>
+              <span>Major:</span> Kompiuterine fizika ir modeliavimas
+            </p>
+            <p>
+              <span>Minor:</span> Telekomunikacijos ir inzinerija
+            </p>
+          </div>
+        </section>
+        <section className="Section">
+          <div className="Section--left">
+            <h3>Skills</h3>
+          </div>
+          <div className="Section--right">
+            <div>
+              <h3>Offise skills:</h3>
+              <p>Dancing, physics, snowboarding</p>
+            </div>
+            <div>
+              <h3>Computer skills:</h3>
+              <p>HTML, CSS, JS, Node.js, React</p>
+            </div>
+          </div>
+        </section>
+        <section className="Section">
+          <div className="Section--left">
+            <h3>Experience</h3>
+          </div>
+          <div className="Section--right">
+            <div>
+              <div className="Section--right--job-title">
+                <h3>CodeAcademy</h3>
+                <span>Best student, 2018.09.11-present</span>
+              </div>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+              </ul>
+            </div>
+            <div>
+              <div>
+                <div className="Section--right--job-title">
+                  <h3>Tomo Petreikio sokiu studija</h3>
+                  <span>Best student, 2010-present</span>
+                </div>
+                <ul>
+                  <li>Valsas</li>
+                  <li>Tango</li>
+                  <li>Salsa</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="Section">
+          <div className="Section--left">
+            <h3>Reference</h3>
+          </div>
+          <div className="Section--right">
+            <p>I need money</p>
+          </div>
+        </section>
+      </main>
+    </section>
   );
 }
 
